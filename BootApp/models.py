eaa980ce791996a007f426ba6b3501062b559b05
shree <shreekrishnaprajapati5@gmail.com>
2023-06-01 20:00:24 +0530

    Initial commit

---
+++ BootApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+	nsubject = models.CharField(max_length=120)
+	ntitle = models.CharField(max_length=120)
+	ndesc = models.CharField(max_length=200)
